52dcca7205f1f1a32c924182b1808b447e01ac54
Miguel Jimenez <migang9@gmail.com>
2021-08-18 14:51:34 -0400

    Exercise 3 completed

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,7 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+@store3.destroy
+
+puts "The new quantity of stores after deleting the third store is #{Store.count}"
