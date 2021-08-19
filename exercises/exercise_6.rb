604d6d381af1170512db453f23a5765961b57abe
Miguel Jimenez <migang9@gmail.com>
2021-08-19 01:20:17 -0400

    Exercise 7 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,7 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "Ugrram", last_name: "Ruman", hourly_rate: 60)
-@store4.employees.create(first_name: "Evan", last_name: "Peter", hourly_rate: 25)
-@store5.employees.create(first_name: "Tom", last_name: "Pike", hourly_rate: 35)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 50)
+@store1.employees.create(first_name: "Alexa", last_name: "Lowel", hourly_rate: 60)
+@store2.employees.create(first_name: "Kevin", last_name: "Ham", hourly_rate: 70)
+@store2.employees.create(first_name: "Laura", last_name: "Please", hourly_rate: 50)
+@store2.employees.create(first_name: "Michel", last_name: "Jimenez", hourly_rate: 40)
+@store1.employees.create(first_name: "Evan", last_name: "Adkins", hourly_rate: 90)
+@store2.employees.create(first_name: "Tom", last_name: "Claire", hourly_rate: 80)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please provide a store name:"
+useranswer = $stdin.gets.chomp
+
+useranswer = Store.create(name: "#{useranswer}")
+
+useranswer.errors.messages.each do |message|
+  puts message
+end
