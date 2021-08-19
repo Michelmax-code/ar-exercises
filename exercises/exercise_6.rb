a37da80cebf570be65b0f135d20d23dbfc9d12c3
Miguel Jimenez <migang9@gmail.com>
2021-08-19 01:08:28 -0400

    Exercise 6 completed

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Ugrram", last_name: "Ruman", hourly_rate: 60)
+@store4.employees.create(first_name: "Evan", last_name: "Peter", hourly_rate: 25)
+@store5.employees.create(first_name: "Tom", last_name: "Pike", hourly_rate: 35)
